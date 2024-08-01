93aad7ca1833d58835910617c6940618c7f9875d
marcelli <marcelli.faria@gmai.com>
2024-08-01 18:51:40 -0300

    excluir elementos do array

---
+++ excluindo-elementos.js
@@ -0,0 +1,9 @@
+let notas = [ 10, 8.5, 9, 8];
+
+notas.push(10);
+notas.push(11)
+notas.pop();
+
+const media = media = (notas[1] + notas [2] + notas[3] + notas[4] + notas [5])/ notas.length;
+
+console.log (`a nova média de notas do aluno é ${media.toFixed(2)}`);
