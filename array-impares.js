4ccc71fb95efdda32840a5225e4ff8817654de54
marcelli <marcelli.faria@gmai.com>
2024-08-09 14:47:54 -0300

    arrays de números pares e impares

---
+++ array-impares.js
@@ -0,0 +1,7 @@
+const impares = [];
+
+for (let i = 1; i <= 500; i+=2){
+    impares.push(i);
+}
+
+console.log(impares);
---
+++ array-numeros-pares.js
@@ -0,0 +1,7 @@
+const pares = [];
+
+for (let i = 0; i <= 100; i+=2){
+    pares.push(i);
+}
+
+console.log(pares);
